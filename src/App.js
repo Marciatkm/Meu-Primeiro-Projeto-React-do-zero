47b8be4ad878c7a0f7c1080001a38959dc1aafae
unknown <marciatadakuma@gmail.com>
2021-11-19 18:15:49 -0300

    arrumando erros

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1 className="nome-projeto"> Meu Primeiro Projeto React do zero</h1>
-      <h2>  ❤ Meus filmes favoritos,assito sem enjoar. <a class="s2">❤</a></h2>
+      <h1 className="nome-projeto"> Meu Primeiro Projeto React do Zero</h1>
+      <h2>  ❤ Meus filmes favoritos,assisto sem enjoar. <a class="s2">❤</a></h2>
       <MeuPrimeiroReactDoZero/>
       </header>
     </div>
